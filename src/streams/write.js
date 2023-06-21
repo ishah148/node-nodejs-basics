ebc40a8275816289d98968eca564cf3cc3e8a327
ishah <ishah@intervale.ru>
2023-06-21 20:50:40 +0300

    feat: some and calcHsh

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createWriteStream, constants  } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const stream = createWriteStream(filePath, {
+    encoding: 'utf8',
+    flags: constants.O_APPEND | constants.O_CREAT,
+    mode: 0o666
+  });
+
+  console.log('Please enter any text.\nTo terminate the program, press Ctrl + C:\n');
+
+  return new Promise((resolve, reject) => {
+    process.stdin.pipe(stream);
+    stream.on('finish', resolve);
+    stream.on('error', reject);
+  });
 };
 
 await write();
