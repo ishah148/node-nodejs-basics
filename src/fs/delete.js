b59bb88948d28be8d15e59ad46da86cd43543761
ishah <ishah@intervale.ru>
2023-06-21 20:45:59 +0300

    fs: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+    await fs.unlink(filePath);
+    console.log('File deleted successfully.');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+        console.log('FS operation failed. There is no "fileToRemove.txt" file.');
+    } else {
+        console.log(error.code);
+    }
+  }
 };
 
 await remove();
